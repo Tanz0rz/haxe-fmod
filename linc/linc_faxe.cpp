bb4964840fd354a921ec12613565d8e2fe036e6f
Tanner <Shadoninja@gmail.com>
2020-05-02 19:12:30 -0600

    Adding name to the Copyright

--- linc/linc_faxe.cpp
+++ linc/linc_faxe.cpp
@@ -4,6 +4,7 @@
 * The MIT License (MIT)
 *
 * Copyright (c) 2016 Aaron M. Shea
+* Copyright (c) 2020 Tanner Moore
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
