959719532bd4bbda6ee4e374e8d62c11d304ac2d
Apprentice Alchemist <azzer159@yahoo.com>
2020-10-12 16:15:15 +0200

    Should work out of the box now

--- faxe_hl/build.bat
+++ faxe_hl/build.bat
@@ -1,6 +1,5 @@
 @echo off
-IF NOT DEFINED __ARCH__ (
-    @call vcsetup x86
-)
-@cl.exe /Fo"obj/" faxe_hl.cpp -I./ -I../lib/Windows/api/core/inc -I../lib/Windows/api/studio/inc -I%HASHLINK%/include ..\lib\Windows\api\core\lib\%__ARCH__%\fmod_vc.lib ..\lib\Windows\api\studio\lib\%__ARCH__%\fmodstudio_vc.lib libhl.lib /link /DLL /out:faxe.hdll
-@copy faxe.hdll ..\faxe.hdll
+@pushd %~dp0
+@haxelib run hxcpp Build.xml -DHXCPP_M32 %*
+@copy faxe.hdll ..\ndll\Windows\faxe.hdll
+@popd
